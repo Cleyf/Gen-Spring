946ab3de523bf8cf88e7c1ede49edea8093e5a06
Cleyfson <cleyfson12@outlook.com>
2022-01-28 16:34:17 -0300

    Atualizando a assinatirura Repository e a criação da security

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/repository/PostagemRepository.java
@@ -9,5 +9,5 @@ import br.org.generation.blogpessoal.model.Postagem;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
-	List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	 public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 }
